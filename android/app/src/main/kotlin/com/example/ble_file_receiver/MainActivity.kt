ff92101775142459206f653c38900e44b8191cab
Mhmdmkbl2020 <133782007+Mhmdmkbl2020@users.noreply.github.com>
2025-04-27 02:40:58 +0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/ble_file_receiver/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ble_file_receiver/MainActivity.kt
@@ -1,6 +1,6 @@
-// ملف فارغ تمامًا إلا من الاستيرادات الأساسية
 package com.example.ble_file_receiver
-
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {}
+class MainActivity: FlutterActivity() {
+    // No need for override fun onCreate() unless adding custom code
+}

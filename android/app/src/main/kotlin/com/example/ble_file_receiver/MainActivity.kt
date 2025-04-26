76cb4370cc9172be315c445d23b0ecfdcf37096e
Mhmdmkbl2020 <133782007+Mhmdmkbl2020@users.noreply.github.com>
2025-04-27 02:07:42 +0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/ble_file_receiver/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ble_file_receiver/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.ble_file_receiver
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity() // يجب أن يكون فارغًا
